fb8f8bc802a9bce2efda4086a0c62e088fbadba5
beebertaolive <68178210+berta787@users.noreply.github.com>
2021-01-28 15:04:10 -0700

    Tutor HW 19

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,19 @@
-alert('You successfully linked your JavaScript file!');
-console.log("Congrats!");
+// alert('You successfully linked your JavaScript file!');
+// console.log("Congrats!");
+/* jan 28 - tutor - hw 19 */
+$(document).ready(function(){
+    $(".projects").hover(
+        function(){
+            $(".dropdown-menu").slideDown(200);
+        },
+        function(){
+            $(".dropdown-menu").slideUp(200);
 
+        }
+    )
+
+
+
+
+
+})
